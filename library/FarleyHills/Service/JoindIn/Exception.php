348257804ce4057b18da51dd44c9790df83ab325
Shaun Farrell <farrelley@gmail.com>
2010-06-13 19:30:12 -0400

    moved require below comment block

--- library/FarleyHills/Service/JoindIn/Exception.php
+++ library/FarleyHills/Service/JoindIn/Exception.php
@@ -1,11 +1,14 @@
 <?php
-require_once 'Zend/Service/Exception.php';
 
 /**
  * Joind.In Exception
  * @author Shaun Farrell
  *
  */
+
+require_once 'Zend/Service/Exception.php';
+
 class FarleyHills_Service_JoindIn_Exception extends Zend_Service_Exception
 {
+	
 }
