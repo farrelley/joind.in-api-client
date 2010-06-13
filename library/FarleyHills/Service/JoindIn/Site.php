298d773dfcdfb9e1c767f1d707ef05b7bf758369
Shaun Farrell <farrelley@gmail.com>
2010-06-13 19:32:21 -0400

    added phpdocblock comments

--- library/FarleyHills/Service/JoindIn/Site.php
+++ library/FarleyHills/Service/JoindIn/Site.php
@@ -1,24 +1,44 @@
 <?php
 
 /**
- * 
+ * Site - get the current status of the web service
  * @author Shaun Farrell
  *
  */
 
 class FarleyHills_Service_JoindIn_Site extends FarleyHills_Service_JoindIn
 {
+	/**
+	 * api endpoint for site methods
+	 * @var string
+	 */
 	protected static $_endPoint = 'site';
+	
+	/**
+	 * supported site methods
+	 * @var $_supportedMethods array
+	 */
 	protected $_supportedMethods = array(
 		'status',
 	);
 
+	/**
+	 * 
+	 * @param string $username
+	 * @param string $password
+	 * @param string $responseFormat
+	 */
 	public function __construct($username = null, $password = null, $responseFormat = null)
 	{
 		$this->setResponseFormat($responseFormat);
 		parent::__construct($username, $password);
 	}
 	
+	/**
+	 * Get site's current status
+	 * @param string $testString
+	 * @return Zend_Http_Response
+	 */
 	protected function _status($testString)
 	{
 		$methodType = 'status';
@@ -32,5 +52,4 @@ class FarleyHills_Service_JoindIn_Site extends FarleyHills_Service_JoindIn
 		}
 		return $response->getBody();
 	}
-	
 }
